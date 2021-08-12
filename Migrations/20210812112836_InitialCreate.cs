a4a19d8ee4ca7008019674a658a3b30d11103330
darshan-op <74467243+darshan-op@users.noreply.github.com>
2021-08-12 18:47:12 +0530

    Add files via upload
    I have made a  web app with crud operations and this time i have made 2 different tables student and departments and joined them with Id(Student) and DepId(Departments)

---
+++ Migrations/20210812112836_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppCrud.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tbl_Departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_Departments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    First_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Last_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mobile = table.Column<int>(type: "int", nullable: false),
+                    DepId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_Student", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tbl_Departments");
+
+            migrationBuilder.DropTable(
+                name: "tbl_Student");
+        }
+    }
+}
